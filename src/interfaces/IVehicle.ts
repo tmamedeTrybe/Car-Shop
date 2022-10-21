ddc7af98aa7865df7326368d3ea1ca8dd74c83f9
Thiago Mamede <tmamede2@gmail.com>
2022-10-21 15:44:31 -0200

    ADD - Adiciona interface IVehicle

---
+++ src/interfaces/IVehicle.ts
@@ -0,0 +1,16 @@
+import { z } from 'zod';
+
+const vehicleZodSchema = z.object({
+  model: z.string().min(3),
+  year: z.number().int().gte(1900).lte(2022),
+  color: z.string().min(3),
+  status: z.boolean().optional(),
+  buyValue: z.number(),
+});
+
+type IVehicle = z.infer<typeof vehicleZodSchema>;
+
+export {
+  IVehicle,
+  vehicleZodSchema,
+};
