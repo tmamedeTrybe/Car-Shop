a77574b0fda9c484d9d232c2682db4bbdb4242e6
Thiago Mamede <tmamede2@gmail.com>
2022-10-24 17:52:54 -0200

    FIX - Corrigindo chave _v na saída do update

--- src/interfaces/ICar.ts
+++ src/interfaces/ICar.ts
@@ -2,20 +2,21 @@ import { z } from 'zod';
 import { vehicleZodSchema } from './IVehicle';
 
 const carZodSchema = vehicleZodSchema.extend({
-  doorsQty: z
-    .number()
-    .int()
-    .positive()
-    .gte(2)
-    .lte(4),
-  seatsQty: z
-    .number()
-    .int()
-    .positive()
-    .gte(2)
-    .lte(7),
+  doorsQty: z.number({
+    required_error: 'doorsQty is required',
+    invalid_type_error: 'doorsQty must be a number',
+  }).int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positiver number' })
+    .lte(4, { message: 'doorsQty must be less than 4' })
+    .gte(2, { message: 'doorsQty must be greater than 2' }),
+  seatsQty: z.number({
+    required_error: 'seatsQty is required',
+    invalid_type_error: 'seatsQty must be a number',
+  }).int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positiver number' })
+    .lte(7, { message: 'seatsQty must be less than 7' })
+    .gte(2, { message: 'seatsQty must be greater than 2' }),
 });
-
 type ICar = z.infer<typeof carZodSchema>;
 
 export {
--- src/interfaces/IVehicle.ts
+++ src/interfaces/IVehicle.ts
@@ -1,12 +1,33 @@
 import { z } from 'zod';
 
 const vehicleZodSchema = z.object({
-  model: z.string().min(3),
-  year: z.number().int().positive().gte(1900)
-    .lte(2022),
-  color: z.string().min(3),
-  status: z.boolean().optional(),
-  buyValue: z.number().int(),
+  model: z
+    .string({
+      required_error: 'Model is required',
+      invalid_type_error: 'Model must be a string',
+    })
+    .min(3, { message: 'Must be a string with at least 3 characters' }),
+  year: z
+    .number({
+      required_error: 'Year is required',
+      invalid_type_error: 'Year must be a string',
+    })
+    .int({ message: 'Must be a integer' })
+    .positive({ message: 'Must be a positive number' })
+    .gte(1900, { message: 'Year must be greater than 1900' })
+    .lte(2022, { message: 'Year must be less than 2022' }),
+  color: z
+    .string({
+      required_error: 'Color is required',
+      invalid_type_error: 'Color must be a string',
+    })
+    .min(3, { message: 'Must be a string with at least 3 characters' }),
+  status: z.optional((z.boolean({
+    invalid_type_error: 'Status must be a boolean',
+  }))),
+  buyValue: z.number({ required_error: 'BuyValue is required',
+    invalid_type_error: 'BuyValue must be a number' })
+    .int({ message: 'Must be a integer' }),
 });
 
 type IVehicle = z.infer<typeof vehicleZodSchema>;
