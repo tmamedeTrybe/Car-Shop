898fb4b813bb21b6c12aec254c2e388ffba9d891
Thiago Mamede <tmamede2@gmail.com>
2022-10-24 20:23:21 -0200

    FIX - Corrigindo lint

--- src/routes/motorcycleRoutes.ts
+++ src/routes/motorcycleRoutes.ts
@@ -3,16 +3,17 @@ import MotorcycleController from '../controllers/MotorcycleController';
 import MotorcycleModel from '../models/MotorcycleModel';
 import MotorcycleService from '../services/MotorcycleService';
 
-
 const motorcycleRoute = Router();
 
 const motorcycleModel = new MotorcycleModel();
 const motorcycleService = new MotorcycleService(motorcycleModel);
 const carController = new MotorcycleController(motorcycleService);
 
-motorcycleRoute.get('/motorcycles/:id', (req, res) => carController.readOne(req, res));
-motorcycleRoute.put('/motorcycles/:id', (req, res) => carController.update(req, res));
-motorcycleRoute.delete('/motorcycles/:id', (req, res) => carController.delete(req, res));
+const routeId = '/motorcycles/:id';
+
+motorcycleRoute.get(routeId, (req, res) => carController.readOne(req, res));
+motorcycleRoute.put(routeId, (req, res) => carController.update(req, res));
+motorcycleRoute.delete(routeId, (req, res) => carController.delete(req, res));
 motorcycleRoute.post('/motorcycles', (req, res) => carController.create(req, res));
 motorcycleRoute.get('/motorcycles', (req, res) => carController.read(req, res));
 
